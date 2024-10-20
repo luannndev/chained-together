7178d414a451498173e663d25e4614471eb12082
luannndev <106454269+luannndev@users.noreply.github.com>
2024-10-20 22:18:52 +0200

    Add Gradle configurations and update project settings
    Added essential Gradle wrapper script and updated build configurations to support new libraries and plugins. Introduced IntelliJ IDEA project files, including Gradle settings and migration, and adjusted JDK version to 21. Configured repositories, dependencies, and relevant tasks for project compilation and execution.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,12 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.api.JavaVersion.VERSION_21
+import xyz.jpenilla.runpaper.task.RunServer
+
 plugins {
-    id("java")
+    java
+    alias(libs.plugins.shadow)
+    alias(libs.plugins.paperweight.userdev)
+    alias(libs.plugins.run.paper)
 }
 
 group = "dev.luan.chained"
@@ -7,13 +14,54 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://jitpack.io")
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(rootProject.libs.lombok)
+    annotationProcessor(rootProject.libs.lombok)
+    implementation(rootProject.libs.guice)
+    implementation(rootProject.libs.annotations)
+    implementation(rootProject.libs.classindex)
+    annotationProcessor(rootProject.libs.classindex)
+
+    paperweight.paperDevBundle("1.21.1-R0.1-SNAPSHOT")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.withType<JavaCompile>().configureEach {
+    sourceCompatibility = VERSION_21.toString()
+    targetCompatibility = VERSION_21.toString()
+    options.encoding = "UTF-8"
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+
+    named<Jar>("jar") {
+        archiveFileName.set("${project.name}-${project.version}.jar")
+    }
+
+    named<ShadowJar>("shadowJar") {
+        archiveFileName.set("${project.name}-${project.version}-all.jar")
+    }
+
+    named<ProcessResources>("processResources") {
+        from(sourceSets.main.get().resources.srcDirs) {
+            filesMatching("paper-plugin.yml") {
+                expand("version" to project.version)
+            }
+            duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        }
+    }
+
+    named<RunServer>("runServer") {
+        minecraftVersion("1.21.1")
+    }
+
+    build {
+        dependsOn(named("shadowJar"))
+    }
 }
